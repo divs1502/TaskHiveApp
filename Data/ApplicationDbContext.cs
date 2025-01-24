bd92b0cde15fcfb7815a4246f51d04b5318a4990
Divyesh Lahane <93811673+divs1502@users.noreply.github.com>
2025-01-25 00:20:17 +0530

    Removed unnecessary fields

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,11 +10,5 @@ namespace TaskHiveSyncs.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Post> Posts { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // Make sure to use the correct case
-            modelBuilder.Entity<User>().ToTable("users");  // Use quotes if necessary
-            modelBuilder.Entity<Post>().ToTable("posts");  // Use quotes if necessary
-        }
     }
 }
